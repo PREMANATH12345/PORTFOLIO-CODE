eae57f960b3c5368d59688b7409ded76cf800512
PREMANATH S <115914831+PREMANATH12345@users.noreply.github.com>
2023-12-08 13:27:08 +0530

    Delete New folder directory

--- New folder/app1/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render,loader
-from django.http import HttpResponse
-
-# Create your views here.
-def htm(request):
-    tem=loader.get_template('aa.html')
-    return HttpResponse(tem.render())
